70823502befbccbfaf4a7eba5cccd56c050bc9c3
elneroz <morenospcs@gmail.com>
2024-08-25 19:28:42 -0500

    fix off mouse hover color; return the original

--- p1/js/gallery.js
+++ p1/js/gallery.js
@@ -20,7 +20,7 @@ function upDate(previewPic){
        1) Update the url for the background image of the div with the id = "image" 
        back to the orginal-image.  You can use the css code to see what that original URL was*/
        
-        document.getElementById('image').style.background = "url(' ')";
+        document.getElementById('image').style.background = "#8e68ff";
 
        /*2) Change the text  of the div with the id = "image" 
        back to the original text.  You can use the html code to see what that original text was
